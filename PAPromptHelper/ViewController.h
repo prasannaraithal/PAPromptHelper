e89b849ac5998f7f85900ece83297983ecf5cfae
prasanna <prasanna@qikwell.com>
2015-11-05 16:24:54 +0530

    Initial Commit

---
+++ PAPromptHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PAPromptHelper
+//
+//  Created by Prasanna on 05/11/15.
+//  Copyright © 2015 Prasanna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PAPromptHelper/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PAPromptHelper
+//
+//  Created by Prasanna on 05/11/15.
+//  Copyright © 2015 Prasanna. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
